489fec613c28c317bda81620f45a53653e7d4cf3
Subhadiptech <subhadipdhn@gmail.com>
2022-08-13 18:38:59 +0530

    splash working done

--- app/src/main/java/com/ersubhadip/liveedgedetectionsdk/activities/SplashActivity.kt
+++ app/src/main/java/com/ersubhadip/liveedgedetectionsdk/activities/SplashActivity.kt
@@ -1,11 +1,30 @@
 package com.ersubhadip.liveedgedetectionsdk.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.animation.Animation
+import androidx.appcompat.app.AppCompatActivity
+import com.ersubhadip.liveedgedetectionsdk.databinding.ActivitySplashBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    private lateinit var animationUp: Animation
+    private lateinit var animationDown: Animation
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        animationUp = AnimationUtils.loadAnimation(this@SplashActivity,)
+
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(3000L)
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
