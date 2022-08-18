ada0474d3b7b087e64b5bef65f74fccfde77baec
Subhadiptech <subhadipdhn@gmail.com>
2022-08-19 03:18:26 +0530

    openCV setup done

--- app/src/main/java/com/ersubhadip/liveedgedetectionsdk/activities/MainActivity.kt
+++ app/src/main/java/com/ersubhadip/liveedgedetectionsdk/activities/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         changeFragment(HomeFragment())
 
+
     }
 
     private fun changeFragment(fragment: Fragment) {
